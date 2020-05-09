e402768e1d371e0be1e220c55c67ef8bee843d13
quyetdeptrai <quyetqtm3.qb@gmail.com>
2020-05-10 00:05:19 +0700

    seeding databasev2

---
+++ SupportWeb.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupportWeb.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportWeb.Data.Extensions
+{
+   public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NhanSu>().HasData(
+            new NhanSu()
+            {
+                Ma=new Guid ("AED723A2-9AD3-4F2A-8753-DA1B06EA5520"),
+                HoTen ="Đinh Xuân Quyết",
+                SoDienThoai=0963555396,
+                BoPhan=Enums.BoPhan.Support,
+                ChucVu="Leader",
+            },
+            new NhanSu()
+            {
+                Ma=new Guid("FBA9D758-5204-4E31-A2EA-D1466E947F23"),
+                HoTen = "Trần Gia Bảo",
+                SoDienThoai = 0,
+                BoPhan = Enums.BoPhan.Support,
+                ChucVu = "Leader",
+            }
+        );
+        }
+    }
+}
---
+++ SupportWeb.Data/Migrations/20200508175009_SeedData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SupportWeb.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "NhanSu",
+                columns: new[] { "Ma", "BoPhan", "ChucVu", "HoTen", "NgaySinh", "SoDienThoai" },
+                values: new object[] { new Guid("aed723a2-9ad3-4f2a-8753-da1b06ea5520"), 3, "Leader", "Đinh Xuân Quyết", null, 963555396 });
+
+            migrationBuilder.InsertData(
+                table: "NhanSu",
+                columns: new[] { "Ma", "BoPhan", "ChucVu", "HoTen", "NgaySinh" },
+                values: new object[] { new Guid("fba9d758-5204-4e31-a2ea-d1466e947f23"), 3, "Leader", "Trần Gia Bảo", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "NhanSu",
+                keyColumn: "Ma",
+                keyValue: new Guid("aed723a2-9ad3-4f2a-8753-da1b06ea5520"));
+
+            migrationBuilder.DeleteData(
+                table: "NhanSu",
+                keyColumn: "Ma",
+                keyValue: new Guid("fba9d758-5204-4e31-a2ea-d1466e947f23"));
+        }
+    }
+}
